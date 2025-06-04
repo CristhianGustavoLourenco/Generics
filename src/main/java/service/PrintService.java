b36efba850ad8b3e057072aaaa0faa7bed05c5aa
CristhianGustavoLourenco <cristhianlourenco90@gmail.com>
2025-06-03 22:36:05 -0300

    Postagem da Atividade Generics

---
+++ src/main/java/service/PrintService.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package service;
+
+/**
+ *
+ * @author crist
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintService<T> {
+
+	private List<T> list = new ArrayList<>();
+
+	public void addValue(T value) {
+		list.add(value);
+	}
+
+	public T first() {
+		if (list.isEmpty()) {
+			throw new IllegalStateException("List is empty");
+		}
+		return list.get(0);
+	}
+
+	public void print() {
+		System.out.print("[");
+		if (!list.isEmpty()) {
+			System.out.print(list.get(0));
+		}
+		for (int i = 1; i < list.size(); i++) {
+			System.out.print(", " + list.get(i));
+		}
+		System.out.println("]");
+	}
+}
